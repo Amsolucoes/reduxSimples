fc1944787fb4750d19fbc737245bc82ee24be7f6
andreavelino02 <andre.avelino@ilegra.com>
2023-04-22 10:12:03 -0400

    projeto inicial redux simples pronto

---
+++ src/components/Intervalo.jsx
@@ -0,0 +1,47 @@
+/* eslint-disable import/no-anonymous-default-export */
+import "./Intervalo.css";
+import React from "react";
+import Card from "./Card";
+import { connect } from "react-redux";
+import { alterarNumeroMinimo, alterarNumeroMaximo } from "../store/actions/numerosAction";
+
+function Intervalo(props) {
+  const { max, min } = props;
+
+  return (
+    <Card title="Intervalo de Números" red>
+      <div className="Intervalo">
+        <span>
+          <strong>Mínimo:</strong>
+          <input type="number" value={min} onChange={e => props.alterarMinimo(+e.target.value)} />
+        </span>
+        <span>
+          <strong>Máximo:</strong>
+          <input type="number" value={max} onChange={e => props.alterarMaximo(+e.target.value)} />
+        </span>
+      </div>
+    </Card>
+  );
+}
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max,
+  };
+}
+
+function mapDispatchToProps(dispatch) {
+  return {
+    alterarMinimo(novoNumero) {
+      const action = alterarNumeroMinimo(novoNumero);
+      dispatch(action);
+    },
+    alterarMaximo(novoNumero) {
+      const action = alterarNumeroMaximo(novoNumero);
+      dispatch(action)
+    }
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(Intervalo);
---
+++ src/components/Sorteio.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import Card from "./Card";
+import { connect } from "react-redux";
+
+function Sorteio(props) {
+  const {max, min} = props
+  const aleatorio = parseInt(Math.random() * (max - min)) + min;
+
+  return (
+    <Card title="Sorteio de uns Números" purple>
+      <div>
+        <span>
+            <span>Resultado:</span>
+          <strong>{aleatorio}</strong>
+        </span>
+      </div>
+    </Card>
+  );
+};
+
+function mapStateToProps(state) {
+  return {
+    min: state.numeros.min,
+    max: state.numeros.max
+  }
+}
+
+export default connect(mapStateToProps)(Sorteio)
